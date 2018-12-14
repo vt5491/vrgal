64f46e6906aa7070c71dfe932e4540c47f543daa
Vince Turner <vt5491@gmail.com>
2018-12-13 20:31:52 -0800

    v0.8.3. Read .pem files from etc in meta-data-proxy, so you no longer have to copy them to the servers directory. I had updated the letsencrypt certs and forgot to copy them and started receiving CORS errors.

--- servers/meta-data-proxy/index.js
+++ servers/meta-data-proxy/index.js
@@ -1,27 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors')
-const fs = require('fs');
+const cors = require("cors");
+const fs = require("fs");
 const http = require("http");
 const https = require("https");
-const request=require('request');
-const bodyParser = require('body-parser');
+const request = require("request");
+const bodyParser = require("body-parser");
 const port = 1337;
 //const port = 3000;
-const util = require('util')
+const util = require("util");
 
-let readBasePath = 'data/examples';
-let tmpPath = 'tmp/examples';
-let appPrefix = 'vrize';
+let readBasePath = "data/examples";
+let tmpPath = "tmp/examples";
+let appPrefix = "vrize";
 let keyPath, certPath, user;
 
 var options = {
-  host: 'localhost',
+  host: "localhost",
   port: 3000,
-  path: '/examples/all_lifted',
-  method: 'GET',
+  path: "/examples/all_lifted",
+  method: "GET",
   headers: {
-    'Content-Type': 'application/json'
+    "Content-Type": "application/json"
   }
 };
 
@@ -30,27 +30,33 @@ var metaDataServer = "http://localhost:3000";
 
 user = process.env.USER;
 console.log(`meta-data-proxy: entered, user=${user}`);
-if (user === 'ubuntu') {
+if (user === "ubuntu") {
   // production path
-  keyPath ='servers/meta-data-proxy/certs/privkey2.pem';  
-  certPath = 'servers/meta-data-proxy/certs/fullchain2.pem';
-}
-else {
+  // keyPath ='servers/meta-data-proxy/certs/privkey2.pem';
+  // certPath = 'servers/meta-data-proxy/certs/fullchain2.pem';
+  keyPath = "/etc/letsencrypt/live/infinitewheelie.org/privkey.pem";
+  certPath = "/etc/letsencrypt/live/infinitewheelie.org/fullchain.pem";
+} else {
   // dev path
-  keyPath = 'servers/meta-data-proxy/certs/localhost.key';  
-  certPath = 'servers/meta-data-proxy/certs/localhost.crt';  
+  keyPath = "servers/meta-data-proxy/certs/localhost.key";
+  certPath = "servers/meta-data-proxy/certs/localhost.crt";
 }
-https.createServer({
-    //key: fs.readFileSync('servers/meta-data-proxy/certs/localhost.key'),
-    //key: fs.readFileSync('/etc/letsencrypt/archive/infinitewheelie.org/privkey2.pem'),
-    //key: fs.readFileSync('servers/meta-data-proxy/certs/privkey2.pem'),
-    key: fs.readFileSync(keyPath),
-    //cert: fs.readFileSync('servers/meta-data-proxy/certs/localhost.crt')
-    //cert: fs.readFileSync('/etc/letsencrypt/archive/infinitewheelie.org/cert2.pem')
-    //cert: fs.readFileSync('servers/meta-data-proxy/certs/cert2.pem')
-    //cert: fs.readFileSync('servers/meta-data-proxy/certs/fullchain2.pem')
-    cert: fs.readFileSync(certPath)
-  }, app).listen(port);
+https
+  .createServer(
+    {
+      //key: fs.readFileSync('servers/meta-data-proxy/certs/localhost.key'),
+      //key: fs.readFileSync('/etc/letsencrypt/archive/infinitewheelie.org/privkey2.pem'),
+      //key: fs.readFileSync('servers/meta-data-proxy/certs/privkey2.pem'),
+      key: fs.readFileSync(keyPath),
+      //cert: fs.readFileSync('servers/meta-data-proxy/certs/localhost.crt')
+      //cert: fs.readFileSync('/etc/letsencrypt/archive/infinitewheelie.org/cert2.pem')
+      //cert: fs.readFileSync('servers/meta-data-proxy/certs/cert2.pem')
+      //cert: fs.readFileSync('servers/meta-data-proxy/certs/fullchain2.pem')
+      cert: fs.readFileSync(certPath)
+    },
+    app
+  )
+  .listen(port);
 // http.createServer(function (req, res) {
 // }, app).listen(port);
 // the following line is how to start in http mode.
@@ -76,75 +82,90 @@ var setCors = function(req, res, next) {
   // development. But since the intent is to be widely accessible anyway this
   // would just be overkill (?).
   //
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Request-Method', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Request-Method", "*");
+  res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT");
   //res.setHeader('Access-Control-Allow-Headers', 'content-type, responsetype, write-to-tmp');
-  res.setHeader('Access-Control-Allow-Headers',
-    'content-type, responsetype, write-to-tmp, access-control-allow-origin, access-control-allow-credentials');
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "content-type, responsetype, write-to-tmp, access-control-allow-origin, access-control-allow-credentials"
+  );
 
   next();
-}
+};
 
 // app.use(setCors);
 app.use(cors());
 
-
 // use this
 app.get("/examples/all_lifted.json", (req, resOuter, next) => {
-  console.log('examples/all_lifted route');
-  request.get('http://localhost:3000/examples/all_lifted.json',options,function(err,res,body){
-      if(err) {
+  console.log("examples/all_lifted route");
+  request.get(
+    "http://localhost:3000/examples/all_lifted.json",
+    options,
+    function(err, res, body) {
+      if (err) {
         console.log(`err=${err}`);
-      }
-      else if(res.statusCode !== 200 ) {
+      } else if (res.statusCode !== 200) {
         console.log(`examples/all_lifted: got statusCode=${res.statusCode}`);
-      }
-      else {
+      } else {
         //res.end(body)
         console.log(`examples/all_lifted: got good status code`);
         resOuter.send(body);
       }
-    });
-})
+    }
+  );
+});
 
 app.get("/examples/all_curated.json", (req, resOuter, next) => {
-  request.get(`${metaDataServer}/examples/by_tag.json?tag=mini-gal`,options,function(err,res,body){
-  //request.get(`http://127.0.0.1:3000/examples/by_tag.json?tag=mini-gal`,options,function(err,res,body){
-      if(err) {
+  request.get(
+    `${metaDataServer}/examples/by_tag.json?tag=mini-gal`,
+    options,
+    function(err, res, body) {
+      //request.get(`http://127.0.0.1:3000/examples/by_tag.json?tag=mini-gal`,options,function(err,res,body){
+      if (err) {
         console.log(`err=${err}`);
-      }
-      else if(res.statusCode !== 200 ) {
+      } else if (res.statusCode !== 200) {
         console.log(`examples/all_curated: got statusCode=${res.statusCode}`);
-      }
-      else {
+      } else {
         //res.end(body)
-        console.log(`examples/all_curated: got good status code,body.id=${body.id}, body.example_id=${body.example_id}`);
+        console.log(
+          `examples/all_curated: got good status code,body.id=${
+            body.id
+          }, body.example_id=${body.example_id}`
+        );
         // console.log(`good. resOuter=${util.inspect(resOuter)}`);
         resOuter.send(body);
       }
-    });
-})
+    }
+  );
+});
 
 app.put("/examples/:id/stats/increment.json", (req, resOuter, next) => {
   let id = req.params.id;
   let metric = req.query.metric;
-  console.log(`metaDataServer.1337: now in increment, id=${id}, query=${req.query}, metric=${metric}`);
+  console.log(
+    `metaDataServer.1337: now in increment, id=${id}, query=${
+      req.query
+    }, metric=${metric}`
+  );
   console.log(`query=${util.inspect(req.query)}`);
-  request.put(`${metaDataServer}/examples/${id}/stats/increment.json?${metric}`,options,function(err,res,body){
-      if(err) {
+  request.put(
+    `${metaDataServer}/examples/${id}/stats/increment.json?${metric}`,
+    options,
+    function(err, res, body) {
+      if (err) {
         console.log(`err=${err}`);
-      }
-      else if(res.statusCode !== 200 ) {
+      } else if (res.statusCode !== 200) {
         console.log(`examples/increment: got statusCode=${res.statusCode}`);
-      }
-      else {
+      } else {
         //res.end(body)
         console.log(`examples/increment: got good status code`);
         resOuter.send(body);
       }
-    });
-})
+    }
+  );
+});
 
 app.get("/dummy/abc.json", (req, resOuter, next) => {
   resOuter.send('{"abc": "7"}');
@@ -162,4 +183,4 @@ app.get("/dummy/abc.json", (req, resOuter, next) => {
   //       resOuter.send(body);
   //     }
   //   });
-})
+});
